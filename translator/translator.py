6c5d57125ee9fca742f8272e9326b78996feca6e
Raquel Blanco <raquel.blanco.morago@gmail.com>
2020-03-24 21:32:15 +0100

    >2500 chars now works

--- translator/translator.py
+++ translator/translator.py
@@ -13,32 +13,57 @@ if not endpoint_var_name in os.environ:
 endpoint = os.environ[endpoint_var_name]
 
 
-f = open("text.txt", "r")
-text = f.read()
-
-
-path = '/translate?api-version=3.0'
-params = '&to=de&to=en'
-constructed_url = endpoint + path + params
-
-headers = {
-    'Ocp-Apim-Subscription-Key': subscription_key,
-    'Content-type': 'application/json',
-    'X-ClientTraceId': str(uuid.uuid4())
-}
-
-body = [
-    {"text": text[:2500]} #2500 max size
-]
-
-request = requests.post(constructed_url, headers=headers, json=body)
-response = request.json()
-
-
-out = json.dumps(response, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
-
-f.close()
+def split(text):
+    s = []
+    if len(text) > 2500:
+        pos = 0
+        for i in text:
+            if i == "\n":
+                break
+            pos += 1
+        s.append(text[:pos])
+        
+        if(text[pos + 1:]!=''):
+            s+= split(text[pos + 1:])
+    else:
+        s.append(text)
+    return s
 
 w = open("out.txt", "w")
-w.write(out)
-w.close()
+def translate(r):
+    texts = split(r)
+    first = True
+    for text in texts:
+        path = '/translate?api-version=3.0'
+        params = '&to=de'
+        constructed_url = endpoint + path + params
+
+        headers = {
+            'Ocp-Apim-Subscription-Key': subscription_key,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+
+        body = [
+            {"text": text} #2500 max size
+        ]
+
+        request = requests.post(constructed_url, headers=headers, json=body)
+        response = request.json()
+
+        if first:
+            acc_resp = response
+            first = False
+        else:
+            acc_resp[0]['translations'][0]['text']+="\n"+response[0]['translations'][0]['text']
+
+    out = json.dumps(acc_resp, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
+    w.write(out)
+    w.close()
+
+#Example
+f = open("text.txt", "r")
+r = f.read()
+f.close()
+translate(r)
+#output: out.txt
