35b126e1cc53bc33baadcbf0c64f1cfa74a2b854
RaulMurillo <ramurimontero@gmail.com>
2020-03-27 14:19:07 +0100

    TranslatorText Class

--- src/translator/translator.py
+++ src/translator/translator.py
@@ -4,105 +4,82 @@ import os
 import requests
 import uuid
 import json
-import csv
-
-subscription_key = None
-endpoint = None
-MAX_LENGTH = 2500
+# import csv
 
 
-def check_credentials():
-    key_var_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
-    if not key_var_name in os.environ:
-        raise Exception(
-            'Please set/export the environment variable: {}'.format(key_var_name))
-    subscription_key = os.environ[key_var_name]
+class TranslatorText:
+    def __init__(self, subscription_key, text, lang, orig=None):
+        self._subscription_key = subscription_key
+        self._ttt = text     # Text to translate
+        self._language = lang
+        self._origin = orig
+        self._MAX_LENGTH = 2500
 
-    endpoint_var_name = 'TRANSLATOR_TEXT_ENDPOINT'
-    if not endpoint_var_name in os.environ:
-        raise Exception(
-            'Please set/export the environment variable: {}'.format(endpoint_var_name))
-    endpoint = os.environ[endpoint_var_name]
+    def set_lang(self, lang, orig):
+        self._language = lang
+        self._origin = orig
 
-    return (subscription_key, endpoint)
+    def set_text(self, text):
+        self._ttt = text
 
+    def split(self):
+        """ Split a long text on paragraphs, or lines if needed. """
+        paragraphs = self._ttt.splitlines(keepends=True)
+        for i, p in enumerate(paragraphs):
+            if len(p) > self._MAX_LENGTH:
+                # Split in lines
+                lines = p.split('. ')
+                lines = [l + '. ' for l in lines]
+                paragraphs = paragraphs[:i] + lines + paragraphs[i+1:]
+        return paragraphs
 
-def split(text):
-    s = []
-    if len(text) > MAX_LENGTH:
-        pos = 0
-        for i in text:
-            if i == "\n":
-                break
-            pos += 1
-        s.append(text[:pos])
+    def translate(self):
+        """Translate the text into the given language."""
+        endpoint_var = 'TRANSLATOR_TEXT_ENDPOINT'
+        if not endpoint_var in os.environ:
+            raise Exception(
+                'Please set/export the environment variable: {}'.format(endpoint_var))
+        endpoint = os.environ[endpoint_var]
 
-        if(text[pos + 1:] != ''):
-            s += split(text[pos + 1:])
-    else:
-        s.append(text)
-    return s
+        if len(self._ttt) > self._MAX_LENGTH:
+            trad_in = self._split()
+        else:
+            trad_in = [self._ttt]
 
+        path = '/translate?api-version=3.0'
+        params = '&to='+self._language
+        if self._origin:
+            params += '&from='+self._origin
+        constructed_url = endpoint + path + params
+        headers = {
+            'Ocp-Apim-Subscription-Key': self._subscription_key,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
 
-def translate(text_input, lang='es', orig=None):
-    """Translate a text into a given language.
+        response = [requests.post(constructed_url, headers=headers, json=[
+                                  {"text": text}]).json()[0] for text in trad_in]
 
-    Parameters: 
-        text_input (str): The text to be translated.
-        lang (str): The target languaje to translate the text.
-        orig (str): Origin languaje of text `text_input`.
-    Returns:
-        dict: Equivalent to JSON with the translated text.
-    """
-    subscription_key, endpoint = check_credentials()
+        for r in response[1:]:
+            response[0]['translations'][0]['text'] += r['translations'][0]['text']
 
-    if len(text_input) > MAX_LENGTH:
-        texts = text_input.splitlines(True)
-        for i, t in enumerate(texts):
-            if len(t) > MAX_LENGTH:
-                paragraph = t.split('.')
-                texts = texts[:i] + paragraph + texts[i+1:]
-    else:
-        texts = [text_input]
-
-    path = '/translate?api-version=3.0'
-    params = '&to='+lang
-    if orig:
-        params += '&from='+orig
-    constructed_url = endpoint + path + params
-
-    response = [None] * len(texts)
-    headers = {
-        'Ocp-Apim-Subscription-Key': subscription_key,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
-
-    for i, text in enumerate(texts):
-
-        body = [
-            {"text": text}  # max size
-        ]
-
-        request = requests.post(constructed_url, headers=headers, json=body)
-        response[i] = request.json()[0]
-
-    for r in response[1:]:
-        response[0]['translations'][0]['text'] += r['translations'][0]['text']
-
-    # out_json = json.dumps(response[0], ensure_ascii=False,
-    #                  sort_keys=True, indent=4, separators=(',', ': '))
-    # with open("out.txt", "w") as f:
-    #     f.write(out_json)
-
-    return response[0]
+        return response[0]
 
 
 if __name__ == '__main__':
     # Example
     with open("text.txt", "r") as f:
-        r = f.read()
+        text = f.read()
 
-    translate(r)
-    translate(r, lang='pt', orig='en')
-    #output: out.txt
+    key_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
+    if not key_name in os.environ:
+        raise Exception(
+            'Please set/expot the enviroment variable: {}' .format(key_name))
+    subscription_key = os.environ[key_name]
+
+    app = TranslatorText(subscription_key, text, lang='es')
+    traduction = app.translate()
+    print(traduction)
+    print()
+    app.set_lang(lang='pt', orig='en')
+    print(app.translate())
