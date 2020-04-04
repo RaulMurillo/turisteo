6d938bfb2c0b42c59abef344bc5d7627ba8163d5
Raquel Blanco <raquel.blanco.morago@gmail.com>
2020-04-04 18:25:10 +0200

    Main page (WIP)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,14 +5,78 @@ import {
 	Route,
 	Redirect
 } from "react-router-dom"
-import { Container } from 'reactstrap';
-
+import { Container, Row, Col } from 'reactstrap';
+import ResultPage from './ResultPage';
+import Image from 'react-bootstrap/Image';
+import './style.css';
+import ImageUploader from 'react-images-upload';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 class App extends React.Component {
-
+	constructor(props) {
+        super(props);
+         this.state = { pictures: [] };
+         this.onDrop = this.onDrop.bind(this);
+    }
+ 
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture)
+        });
+	}
+	
 	render() {
 		return (
-				<Container>Hola</Container>
+			<Container>
+				<Row className="justify-content-md-center">
+
+					<Col md="auto">
+						<Image className="logo" src="images/logo_turisteo.png" roundedCircle />
+					</Col>
+
+				</Row>
+				<Row>
+					<ImageUploader
+						withIcon={true}
+						buttonText='Upload image'
+						onChange={this.onDrop}
+						imgExtension={['.jpg', '.gif', '.png', '.gif']}
+						maxFileSize={20971520}
+					/>
+				</Row>
+				<Row className="justify-content-md-center">
+					<Col className="text_lan" sm={4}>
+					Select language:
+					</Col>
+					<Col sm={6}>
+					<ListGroup as="ul" horizontal>
+						<ListGroup.Item as="li" active>
+							English
+  </ListGroup.Item>
+						<ListGroup.Item as="li">
+							Spanish
+						</ListGroup.Item>
+						<ListGroup.Item as="li">
+							Italian
+  </ListGroup.Item>
+						<ListGroup.Item as="li">
+							French
+						</ListGroup.Item>
+						<ListGroup.Item as="li">
+							German
+						</ListGroup.Item>
+					</ListGroup>
+					</Col>
+				</Row>
+				<Router>
+					<Switch>
+						<Route path="/resultpage">
+							<ResultPage />
+						</Route>
+					</Switch>
+				</Router>
+			</Container>
+
 		);
 	}
 }
---
+++ front-end/src/ResultPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+
+class ResultPage extends React.Component {
+    render() {
+
+        return (
+            <Container>Adiós</Container>
+        );
+    }
+}
+
+export default ResultPage;
