ea9c7f5c687017dfb7448a2943976ba6b09d1146
Raul Murillo Montero <ramurimontero@gmail.com>
2020-04-26 21:10:31 +0200

    Updated plot_rectangle.py

--- front-end/app/plot_rectangle.py
+++ front-end/app/plot_rectangle.py
@@ -2,26 +2,26 @@
 # -*- coding: UTF-8 -*-
 from PIL import Image, ImageDraw
 import logging
-import os
 
 
-def plot_rectangle(image, p0, p1, path, color='red'):
+def plot_rectangle(image, p0, p1, color='red'):
     """Plots rectangle in image.
 
     Args:
         image (str): Path of the original image.
-        p0 (tuple): Tuple of `(int, int)` that indicates the first coordinate of the rectangle.
-        p1 (tuple): Tuple of `(int, int)` that indicates the secong coordinate of the rectangle.
+        p0 (dict): Dict with keys 'x' and 'y' that indicates the first coordinate of the rectangle.
+        p1 (dict): Dict with keys 'x' and 'y' that indicates the second coordinate of the rectangle.
     Returns:
         str: Path of the modified image.
     """
-    # get the image
-    img = Image.open(image).convert('RGB')
-    if (len(p0) != 0) and (len(p1) != 0):
-
     # print(im.size)
-        s = min(img.size)//100
-        w = min(10, s)
+    s = min(img.size)//100
+    w = min(10, s)
+
+    try:
+        # get the image
+        img = Image.open(image).convert('RGB')
+        # get the points
         p0 = (p0['x'], p0['y'])
         p1 = (p1['x'], p1['y'])
 
@@ -29,18 +29,19 @@ def plot_rectangle(image, p0, p1, path, color='red'):
         draw.rectangle((p0, p1), fill=None, outline=color, width=w)
         del draw
 
-    # img.show()
-    img_name, extension = image.rsplit('.', 1)
-    # img_name = n[0]
-    # for i in range(1, len(n)-1):
-    #     img_name += '.' + n[i]
+        # img.show()
+        img_name, extension = image.rsplit('.', 1)
 
-    new_img_name = img_name + '_square.' + extension
-    logging.info(new_img_name)
-    new_img_name_base = os.path.basename(new_img_name)
-    path_image = os.path.join(path, 'src', 'instance', 'images', new_img_name_base)
-    img.save(path_image)
-    return path_image
+        new_img_name = img_name + '_square.' + extension
+        logging.info(new_img_name)
+        img.save(new_img_name)
+    except KeyError:
+        logging.warning('Bad points for rectangle.')
+        new_img_name = image
+    except:
+        logging.warning('Unknown error.')
+        new_img_name = image
+    return new_img_name
 
 
 if __name__ == '__main__':
